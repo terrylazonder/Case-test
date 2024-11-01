7620c3295659c9b8801225943f3f3622d05a6421
TheDutchSpartan <59475324+TheDutchSpartan@users.noreply.github.com>
2024-11-01 12:39:21 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,16 @@
-import streamlit as st
-import requests
-import pandas as pd
-import matplotlib
-import matplotlib.pyplot as plt
-import plotly
-import plotly.graph_objects as go
+import streamlit as st 
+import requests 
+import pandas as pd 
+import matplotlib 
+import matplotlib.pyplot as plt 
+import plotly 
+import plotly.graph_objects as go 
 import plotly.express as px
-import numpy
+import numpy 
 import numpy as np
 import time
 
 start_time = time.perf_counter()
-
-# Voegt sidebar toe
-st.sidebar.title('Blog categorieën')
-blog_post = st.sidebar.selectbox(
-    'Selecteer een onderwerp',
-    ('Introductie', 'Procentuele Toename van COVID-19 Gevallen en Sterfgevallen in de EU', 'Procentuele Toename van COVID-19', 'Gediagnosticeerde Gevallen versus Sterfgevallen', 'Data kwaliteit'))
 # API URL for COVID-19 statistics
 url = "https://covid-19-statistics.p.rapidapi.com/reports"
 # Headers for the API request with API key
@@ -25,7 +19,7 @@ headers = {
     'x-rapidapi-key': "89dc140db7mshf142d6f86d6ce14p1c3e02jsn2c6a8f31519e"
 }
 # List of European country codes for the API request
-country_code_EU = ['BEL', 'DNK', 'BGR', 'CYP', 'DEU', 'EST', 'FIN', 'FRA', 'GRC', 'HUN', 'IRL', 'ITA', 'HRV', 'LVA', 'LTU',
+country_code_EU = ['BEL', 'DNK', 'BGR', 'CYP', 'DEU', 'EST', 'FIN', 'FRA', 'GRC', 'HUN', 'IRL', 'ITA', 'HRV', 'LVA', 'LTU', 
                    'LUX', 'MLT', 'NLD', 'AUT', 'POL', 'PRT', 'ROU', 'SVN', 'SVK', 'ESP', 'CZE', 'SWE']
 querystring_EU = {'iso': country_code_EU}
 
@@ -33,14 +27,43 @@ headers = {
     'x-rapidapi-host': "covid-19-statistics.p.rapidapi.com",
     'x-rapidapi-key': "89dc140db7mshf142d6f86d6ce14p1c3e02jsn2c6a8f31519e"
 }
-# Dictionary voor de volledige naam van de map country
+# Dictionary voor de volledige naam van de map country 
 country_names = {
     'BEL': 'België','DNK': 'Denemarken','BGR': 'Bulgarije','CYP': 'Cyprus','DEU': 'Duitsland','EST': 'Estland','FIN': 'Finland',
     'FRA': 'Frankrijk','GRC': 'Griekenland','HUN': 'Hongarije','IRL': 'Ierland','ITA': 'Italië','HRV': 'Kroatië','LVA': 'Letland',
     'LTU': 'Litouwen','LUX': 'Luxemburg','MLT': 'Malta','NLD': 'Nederland','AUT': 'Oostenrijk','POL': 'Polen','PRT': 'Portugal',
     'ROU': 'Roemenië','SVN': 'Slovenië','SVK': 'Slovakije','ESP': 'Spanje','CZE': 'Tsjechië','SWE': 'Zweden'
 }
-    #Makan van een dataframe voor verzamelen van data
+
+# ======================================================================================================================================== #
+# Title and introduction sectie voor de Streamlit app
+st.title("""*COVID-19 Data* van 08 en 09 Maart 2023 voor EU-Landen""")
+
+st.write("""Tijdens de pandemie is het bijhouden van data cruciaal geweest om inzicht te krijgen in de verspreiding en impact van COVID-19 in verschillende regio’s. In dit project hebben we een interactieve data-visualisatie ontwikkeld met behulp van Python en de Streamlit-bibliotheek. Ons doel was om gebruikers de mogelijkheid te geven om de COVID-19-gevallen en sterfgevallen in verschillende Europese landen en hun provincies te verkennen.""")
+st.write("""Hiervoor hebben we als eerst een grafiek gecreëerd die het aantal gediagnosticeerde gevallen en sterfgevallen per land en provincie toont. Gebruikers kunnen zelf kiezen welke data ze willen bekijken, zowel de gediagnosticeerde gevallen als de sterfgevallen, of slechts één van beide datasets. Op deze manier kunnen gebruikers eenvoudig de verspreiding van COVID-19 binnen specifieke regio's analyseren.""")
+
+st.title('Interactief COVID-19 Dashboard voor EU-landen')
+
+st.subheader('Introductie')
+
+st.write("""
+Dit dashboard biedt een overzicht van de verspreiding van COVID-19 in verschillende Europese landen en hun provincies. Het doel van dit dashboard is om gebruikers te voorzien van actuele inzichten over het aantal bevestigde gevallen, sterfgevallen en de procentuele toename van COVID-19 in verschillende regio's.
+
+### Functies van het dashboard:
+1. **COVID-19 Gevallen en Sterfgevallen per Provincie**: Visualiseer het aantal bevestigde gevallen en sterfgevallen voor elke provincie in geselecteerde Europese landen. U kunt eenvoudig wisselen tussen landen om de data voor specifieke regio's te bekijken.
+2. **Procentuele Toename per Dag**: Analyseer de procentuele toename van bevestigde gevallen, actieve gevallen en sterfgevallen tussen twee datums. Dit biedt inzicht in de snelheid waarmee het virus zich verspreidt in verschillende provincies.
+3. **Gediagnosticeerde Gevallen vs. Sterfgevallen**: Vergelijk het aantal bevestigde gevallen met sterfgevallen in elke provincie. Deze visualisatie maakt het mogelijk om te zien welke regio’s harder zijn getroffen door de pandemie.
+   
+### Hoe het dashboard te gebruiken:
+- Selecteer een **land** en **provincie** in de dropdown-menu’s om de data voor een specifieke regio te bekijken.
+- Gebruik de **slider** om data voor verschillende datums te vergelijken.
+- Vink opties aan of uit met de **checkboxes** om specifieke datapunten te tonen of te verbergen.
+
+Het dashboard is ontworpen om overheden, gezondheidsautoriteiten en burgers te helpen bij het beter begrijpen van de impact van COVID-19 in Europa. Door data te visualiseren, kunnen trends gemakkelijker worden geïdentificeerd, wat leidt tot beter geïnformeerde beslissingen.
+""")
+
+# ======================================================================================================================================== #
+# Verzamelen van COVID-19 data voor elk EU country terwijl we gebruik maken van de API
 EU_data = []
 for country_code in country_code_EU:
     querystring_EU = {'iso':country_code}
@@ -53,7 +76,7 @@ for country_code in country_code_EU:
             report['country_name'] = country_names[country_code]
             EU_data.append(report)
 
-#Makan van een dataframe voor verzamelen van data
+#Makan van een dataframe voor verzamelen van data 
 covid_df_EU = pd.DataFrame(EU_data)
 covid_df_EU.set_index('country', inplace = True)
 
@@ -76,6 +99,55 @@ covid_df_EU = covid_df_EU[covid_df_EU['province'] != 'Unknown']
 province_data_EU = covid_df_EU.groupby(['province', 'country_name']).agg({'confirmed': 'sum', 'deaths': 'sum', 'fatality_rate': 'mean'}).reset_index()
 province_data_EU = province_data_EU.reindex(columns=['country_name', 'province', 'confirmed', 'deaths', 'fatality_rate'])
 province_data_EU = province_data_EU.sort_values(by='country_name', ascending=True)
+#Plotly figure
+fig = go.Figure()
+# Toevoegen van Bar traces voor de comfirmed cases en deaths for elke land
+for country in province_data_EU['country_name'].unique():
+    province_data_EU_filtered = province_data_EU[province_data_EU['country_name'] == country]
+    fig.add_trace(go.Bar(x=province_data_EU_filtered['province'],
+                        y=province_data_EU_filtered['confirmed'],
+                        name=f'{country} gediagnosticeerde',
+                        visible=False,
+                        marker_color='blue'))
+    fig.add_trace(go.Bar(x=province_data_EU_filtered['province'],
+                        y=province_data_EU_filtered['deaths'],
+                        name=f'{country} sterfgevallen',
+                        visible=False,
+                        marker_color='red'))
+#Dit maakt het eerste land zijn data visible by defeault
+fig.data[0].visible=True
+fig.data[1].visible=True
+# Dropdown menu voor kiezen van verschillende landen
+dropdown_buttons = []
+
+for country in province_data_EU['country_name'].unique():
+    dropdown_buttons.append({
+        'label':country,
+        'method':'update',
+        'args':[{'visible': [name.startswith(country) for name in [trace.name for trace in fig.data]]},
+        {'title':f'COVID-19 Gegevens voor {country}'}]
+    })
+# Update layout voor het plotly figuur, met een dropdown en titles
+fig.update_layout(
+    title = 'COVID-19 Gediagnosticeerde en Sterfgevallen per Provincie',
+    xaxis_title = 'Provincie',
+    yaxis_title = 'Aantal',
+    barmode = 'group',
+    updatemenus = [{'buttons':dropdown_buttons,
+                    'showactive':True,
+                    'direction':'down'}]
+)
+#weergeven van plot in streamlit
+st.plotly_chart(fig)
+
+# =================================================================================================================================== #
+#Doormiddel van streamlit schrijven we headers en een stuk tekst
+st.header("""Procentuele Toename van COVID-19 Gevallen en Sterfgevallen in de EU""")
+st.write("""De verspreiding van COVID-19 blijft een belangrijke zorg in Europa, waarbij overheden en gezondheidsautoriteiten nauwlettend de dagelijkse stijgingen in besmettingen en sterfgevallen volgen. De onderstaande grafiek biedt een inzichtelijke vergelijking van de procentuele toename van actieve COVID-19-gevallen, bevestigde besmettingen, en sterfgevallen per provincie, tussen 8 en 9 maart 2023.""")
+st.write("""Door deze gegevens te analyseren, krijgen we een duidelijker beeld van welke provincies in verschillende landen het hardst worden getroffen door de pandemie. Dit kan beleidsmakers helpen om beter geïnformeerde beslissingen te nemen over interventies en middelen.""")
+st.write("""Kies hieronder een land en een provincie om de specifieke stijgingspercentages te bekijken. De kleuren in de grafiek geven de stijgingen weer: blauw voor actieve gevallen, oranje voor bevestigde besmettingen, en rood voor sterfgevallen.""")
+
+# =================================================================================================================================== #
 # Berekend verhoogde percentage voor confirmed cases, deaths, en active cases
 covid_df_EU_con_diff = covid_df_EU[['province', 'country_name', 'confirmed', 'confirmed_diff']].copy()
 covid_df_EU_con_diff['2023-03-08'] = covid_df_EU_con_diff['confirmed'] - covid_df_EU_con_diff['confirmed_diff']
@@ -123,40 +195,113 @@ if not province_data_checkbox.empty:
     # Dutch translations for the x-axis labels
     labels = ['Actieve Toename (%)', 'Gediagnosticeerde Toename (%)', 'Sterfgevallen Toename (%)']
     values = province_data_checkbox[['active_increase_%', 'confirmed_increase_%', 'deaths_increase_%']].values[0]
-    # Samenvoegen van verschillende datasets voor de scatter plot
-    covid_df_EU_slider = covid_df_EU[['country_name', 'province']].merge(
-        covid_df_EU_con_diff[['province', 'country_name', '2023-03-08', '2023-03-09']],
-        on=['province', 'country_name'],
-        how='inner').merge(
+# Aanmaken van een staafdiagram met plotly
+    fig_check = go.Figure()
+# Toevoegen van data aan de staafdiagram
+    fig_check.add_trace(go.Bar(
+        x=labels, 
+        y=values,
+        marker_color=['blue', 'orange', 'red']
+    ))
+# Layout van de grafiek instellen
+    fig_check.update_layout(
+        title=f'Toename in Percentage voor {selected_province_checkbox}',
+        xaxis_title='Meting',
+        yaxis_title='Percentage',
+        template='plotly_white',
+        showlegend=False
+    )
+ #Display naam instellen op basis van de geselecteerde land of provincie
+    if selected_province_checkbox == "":
+        display_name = selected_country_checkbox
+    else:
+        display_name = selected_province_checkbox
+# Controle of alle waardes nul zijn (geen toename in data)
+    if np.all(values == 0):
+        st.write(f'Geen toename van covid-19 percentages bekend in {display_name} tussen de dagen 08-03-2023 en 09-03-2023')
+    else:
+        st.plotly_chart(fig_check)
+
+# ================================================================================================================================== #
+# Titel en text voor de tweede sectie van de analyse doormiddel van streamlit
+st.header("""Analyse van COVID-19: Gediagnosticeerde Gevallen versus Sterfgevallen""")
+st.write("""Het verloop van de COVID-19-pandemie kan per regio sterk verschillen, afhankelijk van verschillende factoren zoals bevolkingsdichtheid, zorgcapaciteit en overheidsmaatregelen. Het begrijpen van deze regionale verschillen is essentieel voor zowel beleidsmakers als gezondheidsautoriteiten.""")
+st.write("""De onderstaande grafiek biedt een visuele weergave van het aantal gediagnosticeerde gevallen in verhouding tot het aantal sterfgevallen in Europese provincies, op zowel 8 als 9 maart 2023. De data per dag zijn apart weergegeven, en met de slider kunt u eenvoudig schakelen tussen beide datums. Elke marker vertegenwoordigt een provincie, en de positie ervan toont hoe deze zich verhoudt tot de andere provincies.""")
+st.write("""Door deze informatie te visualiseren, wordt het mogelijk om trends te ontdekken en provincies te identificeren waar sterfgevallen in verhouding tot gediagnosticeerde gevallen hoger zijn, of waar de stijging in besmettingen significant is. Dit type inzicht kan bijdragen aan meer gerichte interventies in de strijd tegen COVID-19.""")
+
+# =================================================================================================================================== #
+# Samenvoegen van verschillende datasets voor de scatter plot
+covid_df_EU_slider = covid_df_EU[['country_name', 'province']].merge(
+    covid_df_EU_con_diff[['province', 'country_name', '2023-03-08', '2023-03-09']],
+    on=['province', 'country_name'],
+    how='inner').merge(
         covid_df_EU_dea_diff[['province', 'country_name', '2023-03-08', '2023-03-09']],
         on=['province', 'country_name'],
         how='inner',
         suffixes=('_con', '_dea')
     )
-    # Filteren van de dataset voor Frankrijk (geen provincies beschikbaar)
-    covid_df_EU_slider = covid_df_EU_slider[
-        ~((covid_df_EU_slider['country_name'] == 'Frankrijk') & (covid_df_EU_slider['province'] == ""))]
-    # Aanmaken van de scatter plot met plotly
-    fig_scat = go.Figure()
-    # Data toevoegen voor 8 maart 2023
-    fig_scat.add_trace(go.Scatter(
-        x=covid_df_EU_slider['2023-03-08_con'],  # X-as: aantal gediagnosticeerde gevallen
-        y=covid_df_EU_slider['2023-03-08_dea'],  # Y-as: aantal sterfgevallen
-        mode='markers',  # Markers (punten) voor elke provincie
-        marker=dict(color=covid_df_EU_slider['country_name'].astype('category').cat.codes),  # Kleuren op basis van land
-        text=covid_df_EU_slider['country_name'],  # Hover-informatie: landnaam
-        name='2023-03-08'  # Legenda label voor deze datum
-    ))
-    # Data toevoegen voor 9 maart 2023
-    fig_scat.add_trace(go.Scatter(
-        x=covid_df_EU_slider['2023-03-09_con'],
-        y=covid_df_EU_slider['2023-03-09_dea'],
-        mode='markers',
-        marker=dict(color=covid_df_EU_slider['country_name'].astype('category').cat.codes),
-        text=covid_df_EU_slider['country_name'],
-        name='2023-03-09',
-        visible=False )) # Standaard onzichtbaar maken van deze data
+# Filteren van de dataset voor Frankrijk (geen provincies beschikbaar)
+covid_df_EU_slider = covid_df_EU_slider[~((covid_df_EU_slider['country_name'] == 'Frankrijk') & (covid_df_EU_slider['province'] == ""))]
+# Aanmaken van de scatter plot met plotly
+fig_scat = go.Figure()
+# Data toevoegen voor 8 maart 2023
+fig_scat.add_trace(go.Scatter(
+    x=covid_df_EU_slider['2023-03-08_con'],# X-as: aantal gediagnosticeerde gevallen
+    y=covid_df_EU_slider['2023-03-08_dea'],# Y-as: aantal sterfgevallen
+    mode='markers',# Markers (punten) voor elke provincie
+    marker=dict(color=covid_df_EU_slider['country_name'].astype('category').cat.codes),  # Kleuren op basis van land
+    text=covid_df_EU_slider['country_name'],  # Hover-informatie: landnaam
+    name='2023-03-08'# Legenda label voor deze datum
+))
+# Data toevoegen voor 9 maart 2023
+fig_scat.add_trace(go.Scatter(
+    x=covid_df_EU_slider['2023-03-09_con'],
+    y=covid_df_EU_slider['2023-03-09_dea'],
+    mode='markers',
+    marker=dict(color=covid_df_EU_slider['country_name'].astype('category').cat.codes),
+    text=covid_df_EU_slider['country_name'],
+    name='2023-03-09',
+    visible=False # Standaard onzichtbaar maken van deze data
+))
+# Layout en slider voor het wisselen tussen 8 en 9 maart
+fig_scat.update_layout(
+    title='Aantal gediagnosticeerde uitgezet tegen het aantal sterfgevallen',
+    xaxis_title='Aantal gediagnosticeerde',
+    yaxis_title='Aantal sterfgevallen',
+    template='plotly_white',
+    sliders=[{
+        'steps': [
+            {
+                'method': 'update',
+                'label': '2023-03-08',
+                'args': [{'visible': [True, False]}, {'title': 'Data van 2023-03-08'}]
+            },
+            {
+                'method': 'update',
+                'label': '2023-03-09',
+                'args': [{'visible': [False, True]}, {'title': 'Data van 2023-03-09'}]
+            }
+        ],
+        'currentvalue': {'prefix': 'Datum: '},# Huidige waarde van de slider
+        'pad': {'t': 50} # Afstand tussen slider en grafiek
+    }]
+)
+# Weergeven van de scatter plot in de app
+st.plotly_chart(fig_scat)
 
+# Tekst omtrent data kwaliteit
+st.subheader('Discussie over Data Kwaliteit')
+st.write("""
+De dataset die in deze analyse is gebruikt, bevat geen ontbrekende waarden, wat betekent dat alle statistieken volledig zijn vertegenwoordigd. Dit vormt een solide basis voor een nauwkeurige analyse van COVID-19 gevallen en sterfgevallen.
+
+Daarnaast zijn mogelijke uitschieters gemarkeerd tijdens de verkenning, wat kan duiden op provincies met ongewoon hoge of lage aantallen gevallen. Deze uitschieters zijn opgenomen in de analyse, maar worden gemarkeerd voor verdere beoordeling.
+
+Door de volledigheid en betrouwbaarheid van de data te waarborgen, zullen de inzichten die uit dit dashboard worden getrokken waarschijnlijk de ware COVID-19 trends in de Europese regio’s weerspiegelen.
+""")
+#Disclaimer
+
+st.subheader('Disclaimer')
+st.write("""Ook al bevat de dataset geen ontbrekenden waarden, zijn de provincies niet altijd accuraat. Zo zijn er EU-landen die wel provincies/regio's bevatten, maar dat niet is aangegeven in de dataset. Zo lijkt het dus alsof sommige landen geen provincies hebben terwijl dit wel het geval is.""")
 end_time = time.perf_counter()
 
 print(f"Total execution time: {end_time - start_time} seconds")
